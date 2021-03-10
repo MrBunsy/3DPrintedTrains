ff841ca7f2cf47f6cdb73006a61c747dca0e7925
Luke Wallin <luke@lukewallin.co.uk>
2021-03-10 17:33:21 +0000

    split python up a bit to re-use elsewhere

--- generate.py
+++ generate.py
@@ -25,6 +25,7 @@ import argparse
 import numpy
 import stl
 from stl import mesh
+from OpenSCADJob import JobDescription, multiprocessJobs
 
 Path("out").mkdir(parents=True, exist_ok=True)
 
@@ -34,47 +35,9 @@ highcubes = [True, False]
 coins = ["penny", "tuppence", "none"]
 
 
-class JobDescription():
-    def __init__(self, scad, filename):
-        self.variables = {}
-        self.scad = scad
-        self.filename = filename
-
-    def addVariable(self, name, value):
-        if isinstance(value, str):
-            self.variables[name] = "\\\"{}\\\"".format(value)
-        elif type(value) == bool:
-            self.variables[name] = "true" if value else "false"
-        else:
-            self.variables[name] = value
-
-    def addVariables(self, dict):
-        '''
-        given dict of ["variable name"] = value, add them all
-        :param dict:
-        :return:
-        '''
-        for key in dict:
-            self.addVariable(key, dict[key])
-
-    def getVariableString(self):
-        return " ".join(
-            ["-D {varname}={varvalue}".format(varname=key, varvalue=self.variables[key]) for key in self.variables])
-
-    def do(self):
-        cmd = "openscad -o out/{filename}.stl {variablestring} {scad}".format(filename=self.filename,
-                                                                              variablestring=self.getVariableString(),
-                                                                              scad=self.scad)
-        print(cmd)
-        os.system(cmd)
-        print("finished {}".format(self.filename))
-        model = mesh.Mesh.from_file("out/{filename}.stl".format(filename=self.filename))
-        model.save("out/{filename}-binary.stl".format(filename=self.filename), mode=stl.Mode.BINARY)
-        # TODO can even use matplotlib to render it!
 
 
-def executeJob(job):
-    job.do()
+
 
 
 def class66_jobs():
@@ -277,5 +240,4 @@ if __name__ == '__main__':
     if args.wheels:
         jobs.extend(wheel_jobs())
 
-    p = Pool(multiprocessing.cpu_count() - 1)
-    p.map(executeJob, jobs)
+    multiprocessJobs(jobs)
